a4a939f7f0e7eee2129748045021e62087441b91
ohmybiino <davenarzobal@gmail.com>
2024-12-03 08:24:54 +0800

    Add Application Data migration and Add/Delete feature for ManageBook

--- Controllers/ManageBooksController.cs
+++ Controllers/ManageBooksController.cs
@@ -1,12 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManagement.Database;
+using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagement.Controllers
 {
     public class ManageBooksController : Controller
     {
-        public IActionResult ManageBooksPage()
+        private readonly ApplicationDbContext _context;
+
+        public ManageBooksController(ApplicationDbContext context) 
         {
+            _context = context;
+        }
+        public async Task <IActionResult> ManageBooksPage()
+        {
+            var bookList = await _context.Books.ToListAsync();
+
+            return View(bookList);
+        }
+
+        //CREATE
+        public IActionResult CreateBook() 
+        {
+
             return View();
+        }
+
+        //CREATE: POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateBook([Bind("Title,Description,Author,Genre,ISBN,isBorrowed")] BookModel book) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ManageBooksPage));
+            }
+            return View(book);
+        }
+
+        //GET: DELETE
+        [HttpGet]
+        public async Task<IActionResult> DeleteBook(string? id) 
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(b => b.ISBN == id);
+
+            if (book == null) 
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //POST: DELETE
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteBookConfirmed(string id) 
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book != null) 
+            {
+                _context.Books.Remove(book);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(ManageBooksPage));
+        }
+
+        private bool MovieExists(string id) 
+        {
+            return _context.Books.Any(b => b.ISBN == id);
         }
     }
 }
---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<LibraryManagement.Models.BookModel> Books { get; set; }
+    }
+}
